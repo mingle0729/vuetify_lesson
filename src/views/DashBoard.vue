1640e27eb001b89e8d48ddf9a3a1d0ac28b5a482
kimmj <mjjkim@sysoffing.co.kr>
2022-08-17 18:14:48 +0900

    grid system practice

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,13 +1,19 @@
 <template>
-  <div>
-    DashBoard
-  </div>
+  <v-container
+    class="red"
+    fluid
+  >
+    <!-- fluid: 가로폭 max기능-->
+    <v-card class="yellow pa-3 ">
+      대시보드
+    </v-card>
+  </v-container>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style>
-    
+
 </style>
--- src/views/GridSystem.vue
+++ src/views/GridSystem.vue
@@ -1,13 +1,43 @@
 <template>
-  <div>
-    GridSystem
-  </div>
+  <v-container
+    fluid
+  >
+    <!-- fluid: 가로폭 max기능-->
+    <v-card class="pa-3 ">
+      <v-row>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card
+            class="pa-3"
+            outline
+          >
+            Col
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style>
-    
+
 </style>
